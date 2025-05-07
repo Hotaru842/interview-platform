36a0a736a31f16a24ddd5152869328ef8f71d07e
UnicornDad8 <unicorndad8@gmail.com>
2025-05-07 17:58:25 -0300

    Inicializar shadcn en el proyecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Hola mundo</h1>
+      <Button>Sign in</Button>
     </div>
   );
 }

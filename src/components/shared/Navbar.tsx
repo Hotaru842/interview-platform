2624c2e208f46a42ecfa8772d656b285f10ce280
UnicornDad8 <unicorndad8@gmail.com>
2025-05-14 16:30:39 -0300

    Agregar logo a la barra de navegacion y favicon

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { ModeToggle } from '@/components/ui/ModeToggle';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from "../../images/logo.svg";
 
 const Navbar = () => {
   return (
-    <div>Navbar
-      <ModeToggle />
-    </div>
+    <nav className="border-b">
+      <div className="flex h-16 items-center px-4 container mx-auto">
+        <Link href="/" className="flex items-center gap-2 mr-6 hover:opacity-80 transition-opacity">
+          <Image 
+            src={logo}
+            alt="logo"
+            className="size-8"
+          />
+          <div className="flex items-center gap-1">
+            <span className="text-2xl font-black text-[#4004fc]">Code</span>
+            <span className="text-md font-normal text-gray-700 dark:text-white">sync</span>
+          </div>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
